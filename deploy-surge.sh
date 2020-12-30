a1c0b8c76369750aec6e95b7c4a9593b98578a35
ducthai127 <nguyenducthai127@gmail.com>
2020-12-30 15:48:51 +0700

    init src

---
+++ deploy-surge.sh
@@ -0,0 +1,11 @@
+# Build
+npm run build
+
+# Move to build folder
+cd build
+
+# Clone index.html into 200.html
+cp index.html 200.html
+
+# Start deploying via Surge
+surge . thai-todolist.surge.sh
---
+++ src/components/BackGround.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BackGround() {
+  return (
+    <div className="background-sky">
+      <div className="stars1"></div>
+      <div className="stars2"></div>
+      <div className="stars3"></div>
+      <div className="shooting-stars1"></div>
+      <div className="shooting-stars2"></div>
+      <div className="shooting-stars3"></div>
+    </div>
+  );
+}
